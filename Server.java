2b35f588c63988ca5f7df2da4249a0cefb466ad9
Davidwijayanto04 <115697663+Davidwijayanto04@users.noreply.github.com>
2022-11-01 16:16:26 +0700

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,86 @@
+
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO Create Database
+
+        // TODO Create Menu
+
+        // TODO Insert Menu to Database
+
+        // TODO Display Main Menu and User must be Register First
+
+        // TODO Create User from register in Main Menu
+
+        // TODO Display Menu
+
+    }
+}
+public class Server {
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+        
+
+        String nama;
+        int golongan;
+        float harga=0;
+        float totalharga, pilihan, menu, dessert, nomer;
+
+        System.out.println("Selamat datang di restoran EAD");
+        System.out.println("Silahkan register terlebih dahuluu");
+        System.out.println("=====================================");
+        System.out.println("masukkan nama: ");
+        nama=input.nextInt();
+        System.out.println("No. Hnadphone");
+        nomer=input.nextInt();
+
+        System.out.println("=====================================");
+        System.out.println("1. Menu");
+        System.out.println("2. Pilih Menu");
+        System.out.println("3. Keluar");
+
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+}
+
+        switch(golongan){
+            case 1 :
+            System.out.println("1. Baso (Makanan) Rp. 10000");
+            System.out.println("2. Es Teh (Minuman) Rp. 5000");
+            System.out.println("3. Brownies (Dessert) Rp. 15000");
+
+            case 2 :
+            System.out.println("Masukkan Pilihan");
+            pilihan=input.nextInt();
+            System.out.println("Masukkan nama menu");
+            menu=input.nextInt();
+                
+            
+            case 3 :
+            System.out.println("Masukkan nama menu");
+            break;
+
+
+        };
+        
+
+
+
+
+
+
+    
+    }
+
+
+}
